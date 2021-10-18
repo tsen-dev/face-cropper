ec31ad1bf3596b4b886735f284d8241c24cad5c7
Toprak Sen <toprak285@hotmail.com>
2021-10-18 01:19:48 +0100

    Refactor landmark rotating code into new function rotate_landmarks

--- normalised_face_cropper.py
+++ normalised_face_cropper.py
@@ -7,6 +7,7 @@ class NormalisedFaceCropper:
 
     left_eye_landmark_indices = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246]
     right_eye_landmark_indices = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398]
+    face_edge_landmark_indices = [234, 152, 454, 10]
 
     def __init__(self):
         """
@@ -36,48 +37,21 @@ class NormalisedFaceCropper:
                 (image_width, image_height)
             )
 
-            im = image.copy()
-            cv2.circle(im, (left_eye_centre[0], image_height - left_eye_centre[1]), 5, (0, 255, 0))
-            cv2.circle(im, (right_eye_centre[0], image_height - right_eye_centre[1]), 5, (255, 255, 255))
             m = (
-                int((right_eye_centre[0] + left_eye_centre[0]) / 2),
-                image_height - 1 - int((right_eye_centre[1] + left_eye_centre[1]) / 2)
+                round((right_eye_centre[0] + left_eye_centre[0]) / 2),
+                image_height - 1 - round((right_eye_centre[1] + left_eye_centre[1]) / 2)
             )
 
-            for landmark in face_landmarks:
-                cv2.circle(im, (round(landmark.x*image_width), round(landmark.y*image_height)), 1, (0, 0, 255))
+            roll_angle = self.get_face_roll_angle(left_eye_centre, right_eye_centre)
+            rotation_matrix = cv2.getRotationMatrix2D(m, -roll_angle, 1)
+            rotated_image = cv2.warpAffine(image, rotation_matrix, (image.shape[1], image.shape[0]))
 
-            rotation_angle = self.get_in_plane_rotation_angle(left_eye_centre, right_eye_centre)
+            rotated_landmarks = self.rotate_landmarks(
+                [face_landmarks[landmark] for landmark in NormalisedFaceCropper.face_edge_landmark_indices],
+                rotation_matrix,
+                (image_width, image_height))
 
-            rotation_matrix = cv2.getRotationMatrix2D(m, -rotation_angle, 1)
-            image_blac = cv2.warpAffine(image, rotation_matrix, (image.shape[1], image.shape[0]))
-
-            cv2.circle(im, (round(face_landmarks[234].x*image_width), round(face_landmarks[234].y*image_height)), 5, (0, 255, 255))
-            cv2.circle(im, (round(face_landmarks[152].x*image_width), round(face_landmarks[152].y*image_height)), 5, (0, 255, 255))
-            cv2.circle(im, (round(face_landmarks[454].x*image_width), round(face_landmarks[454].y*image_height)), 5, (0, 255, 255))
-            cv2.circle(im, (round(face_landmarks[10].x*image_width), round(face_landmarks[10].y*image_height)), 5, (0, 255, 255))
-
-            left = np.matmul(rotation_matrix, np.array([face_landmarks[234].x*image_width, face_landmarks[234].y*image_height, 1]))
-            bottom = np.matmul(rotation_matrix, np.array([face_landmarks[152].x * image_width, face_landmarks[152].y * image_height, 1]))
-            right = np.matmul(rotation_matrix, np.array([face_landmarks[454].x * image_width, face_landmarks[454].y * image_height, 1]))
-            top = np.matmul(rotation_matrix, np.array([face_landmarks[10].x * image_width, face_landmarks[10].y * image_height, 1]))
-
-            cv2.circle(im, (round(left[0]), round(left[1])), 5, (0, 255, 255))
-            cv2.circle(im, (round(right[0]), round(right[1])), 5, (0, 255, 255))
-            cv2.circle(im, (round(top[0]), round(top[1])), 5, (0, 255, 255))
-            cv2.circle(im, (round(bottom[0]), round(bottom[1])), 5, (0, 255, 255))
-            cv2.rectangle(im, (round(face_landmarks[234].x*image_width), round(face_landmarks[152].y*image_height)), (round(face_landmarks[454].x*image_width), round(face_landmarks[10].y*image_height)), (0, 255, 255))
-
-            imrot = image_blac.copy()
-            cv2.rectangle(im, (round(left[0]), round(bottom[1])), (round(right[0]), round(top[1])), (255, 0, 0))
-            cv2.circle(im, (round(left[0]), round(left[1])), 5, (255, 0, 0))
-            cv2.circle(im, (round(right[0]), round(right[1])), 5, (255, 0, 0))
-            cv2.circle(im, (round(top[0]), round(top[1])), 5, (255, 0, 0))
-            cv2.circle(im, (round(bottom[0]), round(bottom[1])), 5, (255, 0, 0))
-            cv2.imshow("imrot", imrot)
-            cv2.imshow("masked", im)
-
-            return image_blac[round(top[1]):round(bottom[1]), round(left[0]):round(right[0])]
+            return rotated_image[rotated_landmarks[1, 3]:rotated_landmarks[1, 1], rotated_landmarks[0, 0]:rotated_landmarks[0, 2]]
 
 
     def get_left_and_right_eye_centres(self, left_eye_landmarks, right_eye_landmarks, image_size):
@@ -103,30 +77,52 @@ class NormalisedFaceCropper:
 
         return left_eye_centre, right_eye_centre
 
-    def get_in_plane_rotation_angle(self, left_eye_centre, right_eye_centre):
+
+    def get_face_roll_angle(self, left_eye_centre, right_eye_centre):
         """
         Calculate and return the in-plane rotation angle of a face using the centre coordinates of the eyes.
         :param left_eye_centre: (x, y) tuple containing the pixel coordinates of the left eye's centre.
         :param right_eye_centre: (x, y) tuple containing the pixel coordinates of the right eye's centre.
-        :return: The in-plane rotation angle of the face in degrees.
+        :return: The roll angle of the face in degrees.
         """
 
-        if right_eye_centre[1] == left_eye_centre[1]:  # 0 or 180 degree rotation
+        if right_eye_centre[1] == left_eye_centre[1]:  # 0 or 180 degree roll
             if right_eye_centre[0] >= left_eye_centre[0]:
                 return 0
             else:
                 return 180
 
-        elif right_eye_centre[0] == left_eye_centre[0]:  # 90 or 270 degree rotation
+        elif right_eye_centre[0] == left_eye_centre[0]:  # 90 or 270 degree roll
             if right_eye_centre[1] > left_eye_centre[1]:
                 return 90
             else:
                 return 270
 
-        else:  # Rotation between 0 and 360 degrees excluding 0, 90, and 270
+        else:  # Roll between 0 and 360 degrees excluding 0, 90, and 270
             gradient = (right_eye_centre[1] - left_eye_centre[1]) / (right_eye_centre[0] - left_eye_centre[0])
             if right_eye_centre[0] > left_eye_centre[0]:
-                return np.degrees(np.arctan(gradient))  # Rotation between 0 and 90 or 270 and 0 i.e. -90 and 0 degrees
+                return np.degrees(np.arctan(gradient))  # Roll between 0 and 90 or 270 and 0 i.e. -90 and 0 degrees
             else:
-                return 180 + np.degrees(np.arctan(gradient))  # Rotation between 90 and 270 degrees
+                return 180 + np.degrees(np.arctan(gradient))  # Roll between 90 and 270 degrees
+
+
+    def rotate_landmarks(self, landmarks, rotation_matrix, image_size):
+        """
+        Rotate the landmark points using the specified rotation matrix. The new points are rounded to the nearest
+        values and converted to integer types.
+        :param landmarks: The landmarks to be rotated. Must be a list of
+        mediapipe.framework.formats.landmark_pb2.NormalizedLandmark objects.
+        :param rotation_matrix: 3x2 transformation matrix for rotation around a specified point.
+        :param image_size: (width, height) tuple containing the dimensions of the image containing the landmarks
+        :return: A 2xn integer matrix. The first row contains the new x values while the second row contains the new y
+        values. Each column contains the new coordinates for a landmark such that the first column stores the first
+        landmark's new location, and so on.
+        """
+
+        rotated_landmarks = np.matmul(rotation_matrix, np.array(
+                [[landmark.x * image_size[0] for landmark in landmarks],
+                [landmark.y * image_size[1] for landmark in landmarks],
+                [1, 1, 1, 1]]))
+
+        return np.ndarray.astype(np.rint(rotated_landmarks), np.int)
 
