f99cff18a60b3b7be7506ca956a22cd1d5c7fa03
Toprak Sen <toprak285@hotmail.com>
2022-07-28 17:59:57 +0100

    Add demo_video.py

--- demo_video.py
+++ demo_video.py
@@ -4,21 +4,20 @@ from face_cropper import FaceCropper
 face_cropper = FaceCropper()
 
 camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Capture video with default camera (Use DSHOW API for reading to avoid SourceReader warning)
-camera = cv2.VideoCapture('C:/Users/Toprak/Desktop/Katy Perry - Waking Up In Vegas (Official).mp4')  # Read video from specified path
+camera = cv2.VideoCapture('C:/Users/Toprak/Desktop/Pexels Videos 2795168.mp4')  # Read video from specified path
 
 while True:
     read_successful, image_bgr = camera.read()
     if not read_successful: raise RuntimeError('Image could not be read!')
 
-    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
-    face_images_rgb = face_cropper.get_normalised_faces(image_rgb)
+    faces_rgb = face_cropper.get_normalised_faces(cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB))
 
-    if not face_images_rgb: print("No faces detected!")
+    if not faces_rgb:
+        print("No faces detected!")
     else:
-        cv2.imshow('Camera', image_bgr)
-        for face_id, face_image_rgb in enumerate(face_images_rgb):
-            face_image_bgr = cv2.cvtColor(face_image_rgb, cv2.COLOR_RGB2BGR)
-            cv2.imshow('Face {0}'.format(face_id), face_image_bgr)
+        cv2.imshow('Image', image_bgr)
+        for face_id, face_rgb in enumerate(faces_rgb):
+            cv2.imshow('Face {0}'.format(face_id), cv2.cvtColor(face_rgb, cv2.COLOR_RGB2BGR))
 
     if cv2.pollKey() != -1:  # User pressed key
         camera.release()
